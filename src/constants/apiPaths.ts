0e57ff9e8ad5f9680eba2c5e10473449dfdcb467
katerina-kz <kzvolinska@ebsco.com>
2023-02-25 22:41:59 +0200

    task 3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-east-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-east-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-east-1.amazonaws.com/dev",
+  bff: "https://o3kxai97f3.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-east-1.amazonaws.com/dev",
 };
 
