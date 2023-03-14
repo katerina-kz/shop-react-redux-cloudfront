4fb036f733c75e15ab9aabe466682ea2ca5c2f63
katerina-kz <kzvolinska@ebsco.com>
2023-03-14 20:07:21 +0200

    fixes

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -41,7 +41,7 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
     // @ts-ignore
     console.log("File to upload: ", file?.name);
     console.log("Uploading to: ", response.data);
-    const result = await fetch(response.data, {
+    const result = await fetch(response.data.url, {
       method: "PUT",
       body: file,
     });
