c82cec928a9a2362b271b7f7a5af8d2fe015d676
katerina-kz <kzvolinska@ebsco.com>
2023-04-06 15:11:14 +0300

    task 8

--- src/constants/order.tsx
+++ src/constants/order.tsx
@@ -1,10 +1,6 @@
 export enum OrderStatus {
   Open = "OPEN",
-  Approved = "APPROVED",
-  Confirmed = "CONFIRMED",
-  Sent = "SENT",
   Ordered = "ORDERED",
-  Cancelled = "CANCELLED",
 }
 
 export const ORDER_STATUS_FLOW = Object.values(OrderStatus);
